48e706d98ce0182f73cd5ecf9c4a2f83572b0207
diyorbek0512 <bahodirovdiyorbek2007@gmail.com>
2022-07-05 22:28:05 +0500

    Deploy to Heroku

--- data/config.py
+++ data/config.py
@@ -1,15 +1,15 @@
-# from environs import *
-# # # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
-# # # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("IP")  # Xosting ip manzili
-import os
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
-ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
-IP = str(os.environ.get("ip"))  # Xosting ip manzili
-CHANNELS = ["@prezident_maktabiga_tayyorlovv"]
+from environs import *
+# # environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+# # .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("IP")  # Xosting ip manzili
+# import os
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+# ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+# IP = str(os.environ.get("ip"))  # Xosting ip manzili
+#
